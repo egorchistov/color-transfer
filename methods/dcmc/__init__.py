128d93484d8a1a4d445baa6c72cb3aaf6fed7bc5
egorchistov <egorchistov@mail.ru>
2022-09-13 23:32:54 +0300

    Add Data Module for DCMC

---
+++ methods/dcmc/__init__.py
@@ -0,0 +1,16 @@
+def deep_color_mismatch_correction(target, reference):
+    """Deep Color Mismatch Correction in Stereoscopic 3D Images
+
+    Citation
+    --------
+    @inproceedings{croci2021deep,
+      title={Deep Color Mismatch Correction In Stereoscopic 3d Images},
+      author={Croci, Simone and Ozcinar, Cagri and Zerman, Emin and Dudek, Roman and Knorr, Sebastian and Smolic, Aljosa},
+      booktitle={2021 IEEE International Conference on Image Processing (ICIP)},
+      pages={1749--1753},
+      year={2021},
+      organization={IEEE}
+    }
+    """
+
+    return target
