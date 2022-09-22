184922f116cbd4f875381ca9fcfdb66cc0dc80b0
egorchistov <egorchistov@mail.ru>
2022-09-22 11:42:58 +0300

    WIP: SIMP

---
+++ methods/simp/train.py
@@ -0,0 +1,35 @@
+import argparse
+from pathlib import Path
+
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+
+from methods.simp.data import SIMPDataModule
+from methods.simp import SIMP
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset_path", type=str)
+parser.add_argument("--batch_size", type=int)
+parser.add_argument("--img_height", type=int)
+parser.add_argument("--img_width", type=int)
+parser = pl.Trainer.add_argparse_args(parser)
+args = parser.parse_args()
+
+datamodule = SIMPDataModule(
+    Path(args.dataset_path),
+    batch_size=args.batch_size,
+    patch_size=(args.img_height, args.img_width))
+
+model = SIMP()
+
+wandb_logger = WandbLogger(project="simp", log_model="all")
+wandb_logger.watch(model, log="all")
+
+checkpoint = pl.callbacks.ModelCheckpoint(
+    monitor="Photometric Loss",
+    save_last=True,
+    every_n_epochs=5)
+
+trainer = pl.Trainer.from_argparse_args(args, callbacks=[checkpoint], logger=wandb_logger)
+trainer.fit(model, datamodule)
