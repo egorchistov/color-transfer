c822cd89ed2b73381e7f58cb3a0dd90f84327b33
egorchistov <egorchistov@mail.ru>
2022-10-04 16:18:48 +0300

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! WIP: SIMP

--- methods/simp/__init__.py
+++ methods/simp/__init__.py
@@ -100,7 +100,7 @@ class SIMP(pl.LightningModule):
         warped_right = warp_disp(right, -disp)
         valid_mask = F.interpolate(valid_mask[-1][0], scale_factor=4, mode="nearest")
 
-        weights = torch.tensor([1, 6, 6]).reshape(1, 3, 1, 1)
+        weights = torch.tensor([1, 6, 6], device=self.device).reshape(1, 3, 1, 1)
         loss_color_correction = F.smooth_l1_loss(rgb_to_yuv(corrected_left) * weights, rgb_to_yuv(left_gt) * weights)
         loss = loss_color_correction + loss_P + 0.1 * loss_S + loss_PAM_P + loss_PAM_S + loss_PAM_C
 
