dab915229bcb0f24d5711188728215b278138c22
egorchistov <egorchistov@mail.ru>
2023-01-26 16:06:18 +0300

    Revert "Train SIMP with bilinear upsample instead of transposed convs"
    This reverts commit 4597d01e20d0d562e0087b41cbfec038d53097a5.

--- methods/modules.py
+++ methods/modules.py
@@ -288,8 +288,7 @@ class Upsample(torch.nn.Module):
         super().__init__()
 
         self.upsample = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False),
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=not bn),
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1, bias=not bn),
             nn.BatchNorm2d(out_channels) if bn else nn.Identity(),
             nn.LeakyReLU(inplace=True)
         )
