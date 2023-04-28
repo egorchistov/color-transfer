db2be16614380722d24964cb90e10720ccdaec2e
egorchistov <egorchistov@mail.ru>
2023-04-28 15:00:11 +0300

    Rewrite datamodule using stronger augmentations

--- data.py
+++ data.py
@@ -1,125 +1,78 @@
 from pathlib import Path
 
 import torch
-import torchvision.utils
-import pytorch_lightning as pl
-import numpy as np
-import albumentations as A
 import matplotlib.pyplot as plt
 from PIL import Image
-from albumentations.pytorch import ToTensorV2
-from torch.utils.data import Dataset
+from torch.utils import data
+from torchvision.utils import make_grid
+from torchvision.transforms import ColorJitter, RandomCrop
+from torchvision.transforms.functional import pad, crop, to_tensor
+from pytorch_lightning import LightningDataModule
 
 
-distortions = A.OneOf([
-    A.RandomBrightnessContrast(contrast_limit=0, brightness_limit=(-0.3, -0.1)),
-    A.RandomBrightnessContrast(contrast_limit=0, brightness_limit=(0.1, 0.3)),
-    A.RandomBrightnessContrast(contrast_limit=(-0.3, -0.1), brightness_limit=0),
-    A.RandomBrightnessContrast(contrast_limit=(0.1, 0.3), brightness_limit=0),
-    A.RandomGamma(gamma_limit=(70, 90)),
-    A.RandomGamma(gamma_limit=(110, 130)),
-    A.HueSaturationValue(hue_shift_limit=(-30, -10), sat_shift_limit=0, val_shift_limit=0),
-    A.HueSaturationValue(hue_shift_limit=(10, 30), sat_shift_limit=0, val_shift_limit=0),
-    A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=(-30, -10), val_shift_limit=0),
-    A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=(10, 30), val_shift_limit=0),
-    A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=0, val_shift_limit=(-30, -10)),
-    A.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=0, val_shift_limit=(10, 30)),
-], p=36 / 37)
-
-
-class CTDataset(Dataset):
-    def __init__(self, image_dir: Path, transforms, distortions, use_real_distortions):
-        """First distortions are applied to the left image, then transforms are applied to both"""
-
+class Dataset(data.Dataset):
+    def __init__(self, image_dir: Path, crop_size):
+        self.crop_size = crop_size
         self.lefts = sorted(image_dir.glob("*_L.png"))
-        self.lefts_distorted = sorted(image_dir.glob("*_LD.png"))
         self.rights = sorted(image_dir.glob("*_R.png"))
+        self.distortions = ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5)
 
         assert len(self.lefts) == len(self.rights)
-
-        self.transforms = transforms
-        self.distortions = distortions
-        self.use_real_distortions = use_real_distortions
 
     def __len__(self):
         return len(self.lefts)
 
     def __getitem__(self, index):
-        left_gt = np.array(Image.open(self.lefts[index]).convert("RGB"))
-        right = np.array(Image.open(self.rights[index]).convert("RGB"))
+        right = Image.open(self.rights[index]).convert("RGB")
+        left_gt = Image.open(self.lefts[index]).convert("RGB")
 
-        if self.use_real_distortions:
-            left = np.array(Image.open(self.lefts_distorted[index]).convert("RGB"))
-        else:
-            left = left_gt
+        padding = [max(0, self.crop_size[0] - right.size[0]),
+                   max(0, self.crop_size[1] - right.size[1])]
 
-        left = self.distortions(image=left)["image"]
+        right = pad(to_tensor(right), padding)
+        left_gt = pad(to_tensor(left_gt), padding)
 
-        t = self.transforms(image=left, left_gt=left_gt, right=right)
-        left, left_gt, right = t["image"], t["left_gt"], t["right"]
+        crop_params = RandomCrop.get_params(right, output_size=self.crop_size)
 
-        return left, left_gt, right
+        right = crop(right, *crop_params)
+        left_gt = crop(left_gt, *crop_params)
+
+        return self.distortions(left_gt), left_gt, right
 
 
-class CTDataModule(pl.LightningDataModule):
-    def __init__(self, image_dir: Path, batch_size: int, img_height, img_width, num_workers: int, use_real_distortions: bool):
+class DataModule(LightningDataModule):
+    def __init__(self, image_dir: Path, img_width: int, img_height: int, batch_size: int, num_workers: int):
         super().__init__()
 
         self.image_dir = image_dir
+        self.crop_size = (img_width, img_height)
         self.batch_size = batch_size
-        self.img_height = img_height
-        self.img_width = img_width
         self.num_workers = num_workers
-        self.use_real_distortions = use_real_distortions
-
-        self.train = None
-        self.val = None
-        self.pred = None
-
-    def setup(self, stage=None):
-        if stage == "fit" or stage is None:
-            train_transforms = A.Compose([
-                A.PadIfNeeded(self.img_height, self.img_width),
-                A.RandomCrop(self.img_height, self.img_width),
-                A.ToFloat(),
-                ToTensorV2()
-            ], additional_targets={"left_gt": "image", "right": "image"})
-
-            self.train = CTDataset(self.image_dir / "Train", train_transforms, distortions, self.use_real_distortions)
-
-            val_transforms = A.Compose([
-                A.PadIfNeeded(self.img_height, self.img_width),
-                A.CenterCrop(self.img_height, self.img_width),
-                A.ToFloat(),
-                ToTensorV2()
-            ], additional_targets={"left_gt": "image", "right": "image"})
-
-            self.val = CTDataset(self.image_dir / "Validation", val_transforms, distortions, self.use_real_distortions)
 
     def train_dataloader(self):
-        return torch.utils.data.DataLoader(
-            self.train,
+        return data.DataLoader(
+            Dataset(self.image_dir / "Train", self.crop_size),
             batch_size=self.batch_size,
-            shuffle=True,
             num_workers=self.num_workers,
+            pin_memory=True,
+            shuffle=True,
             drop_last=True)
 
     def val_dataloader(self):
-        return torch.utils.data.DataLoader(
-            self.val,
+        return data.DataLoader(
+            Dataset(self.image_dir / "Validation", self.crop_size),
             batch_size=self.batch_size,
-            shuffle=False,
             num_workers=self.num_workers,
+            pin_memory=True,
+            shuffle=False,
             drop_last=False)
 
     def plot_example(self):
-        idx = np.random.choice(len(self.train))
-        left, left_gt, right = self.train[idx]
+        left, left_gt, right = next(iter(self.train_dataloader()))
         residue = (left - left_gt).abs().clamp(0, 1)
-        grid = torchvision.utils.make_grid([left, residue, left_gt, right], nrow=2)
+        grid = make_grid([torch.hstack(column) for column in zip(left, residue, left_gt, right)], nrow=8)
 
-        plt.figure(figsize=(8, 5))
-        plt.title("Left, |Left - Left GT|,\nLeft GT, Right")
+        plt.title("left, abs(left - left_gt), left_gt, right")
         plt.imshow(grid.permute(1, 2, 0))
         plt.xticks([])
         plt.yticks([])
@@ -127,7 +80,5 @@ class CTDataModule(pl.LightningDataModule):
 
 
 if __name__ == "__main__":
-    datamodule = CTDataModule(Path("Artificial Dataset"), batch_size=1, img_height=256, img_width=512, num_workers=0,
-                              use_real_distortions=False)
-    datamodule.setup()
+    datamodule = DataModule(Path("Artificial Dataset"), batch_size=16, img_width=512, img_height=256, num_workers=0)
     datamodule.plot_example()
